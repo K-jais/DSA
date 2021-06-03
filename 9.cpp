b526b25b84c939b028e56e1d9412ecac0fc1d5de
Kush Jaiswal <75881393+K-jais@users.noreply.github.com>
2021-06-03 21:23:13 +0530

    Create 9.cpp

---
+++ 9.cpp
@@ -0,0 +1,17 @@
+int getMinDiff(int arr[], int n, int k) {
+        // code here
+        sort(arr, arr+n);
+        int minele, maxele;
+        int res = arr[n-1]-arr[0];
+        for(int i=1;i<=n-1;i++){
+            if(arr[i]>=k){
+                minele = min(arr[0]+k,arr[i]-k);
+                maxele = max(arr[i-1]+k,arr[n-1]-k);
+                res = min(res,maxele-minele);
+            }
+            else{
+                continue;
+            }
+        }
+        return res;
+    }
