78f84f3cd3bef9893df89b564a9aeb0af179130f
Kush Jaiswal <75881393+K-jais@users.noreply.github.com>
2021-06-17 22:34:19 +0530

    Create 32.cpp

---
+++ 32.cpp
@@ -0,0 +1,11 @@
+class Solution{   
+public:
+    //Function to partition the array around the range such 
+    //that array is divided into three parts.
+    void threeWayPartition(vector<int>& array,int a, int b)
+    {
+        sort(array.begin(),array.end());
+    }
+};
+
+It can also be done with the quick sort technique of two pointers.
